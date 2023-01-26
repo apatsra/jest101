839b80a8dc347183abc692e449fbab881fa96893
ann_apatsra <annwang250643@gmail.com>
2023-01-26 22:11:16 +0700

    add jest test

---
+++ app.js
@@ -0,0 +1,17 @@
+exports.getScore = score => {
+  if (score < 50) {
+    return 'F'
+  }
+  return 'A'
+}
+
+exports.fetchSomeData = () => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve('success'), 100)
+  })
+}
+
+exports.login = async () => {
+  const response = await exports.fetchSomeData()
+  return response === 'success'
+}
